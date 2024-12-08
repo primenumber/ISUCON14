292faf414fa847a01369b486d58d26fec43444ef
yu3mars <yu3mars@users.noreply.github.com>
2024-12-08 16:35:12 +0900

    chairGetNotificationのRetryを1000msにする

--- webapp/go/chair_handlers.go
+++ webapp/go/chair_handlers.go
@@ -208,7 +208,7 @@ func chairGetNotification(w http.ResponseWriter, r *http.Request) {
 	if err := tx.GetContext(ctx, ride, `SELECT * FROM rides WHERE chair_id = ? ORDER BY updated_at DESC LIMIT 1`, chair.ID); err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			writeJSON(w, http.StatusOK, &chairGetNotificationResponse{
-				RetryAfterMs: 100,
+				RetryAfterMs: 1000,
 			})
 			return
 		}
@@ -268,7 +268,7 @@ func chairGetNotification(w http.ResponseWriter, r *http.Request) {
 			},
 			Status: status,
 		},
-		RetryAfterMs: 100,
+		RetryAfterMs: 1000,
 	})
 }
 
